388aaa4c7f69d7249cabad89f04212b972f6b952
James Randall <james@manmachineltd.com>
2023-04-19 12:44:20 +1000

    Incorporated the original brand color

--- resources/js/views/Welcome.vue
+++ resources/js/views/Welcome.vue
@@ -1,29 +1,28 @@
 <script setup>
-import { Head, Link } from '@inertiajs/vue3';
+import {
+    Head,
+} from '@inertiajs/vue3';
 
 defineProps({
-    canLogin: {
-        type: Boolean,
-    },
-    canRegister: {
-        type: Boolean,
-    },
-    laravelVersion: {
-        type: String,
-        required: true,
-    },
-    phpVersion: {
-        type: String,
-        required: true,
-    },
+//
 });
 </script>
 
 <template>
     <Head title="Occompare" />
     <div
-        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white"
+        class="brandBackground relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white"
     >
         <h1 class="text-xl">Something</h1>
     </div>
 </template>
+
+<style scoped>
+/*
+Incorporate the original brand color from the original template:
+'--brand-color': '#fbb040'
+*/
+.brandBackground {
+    background-color: #fbb040;
+}
+</style>
