225e701955aa5b944256dab99054a67d863cddc8
James Randall <james@manmachineltd.com>
2023-04-19 12:38:29 +1000

    added legacy code

---
+++ resources/js/views/Welcome.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <Head title="Occompare" />
+    <div
+        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white"
+    >
+        <h1 class="text-xl">Something</h1>
+    </div>
+</template>
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(['api'])->group(function () {
+    Route::get('/occupations', 'OccupationsController@index');
+    Route::post('/compare', 'OccupationsController@compare');
+});
